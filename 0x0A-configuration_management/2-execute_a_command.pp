de936e45a5d10b24020b9a401302a96031ba5aff
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-22 14:29:46 +0300

    task_2: additional cmd

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,12 @@
-# kills the process killmenow
-exec{'killmenow':
-   command => 'pkill -f killmenow',
+# Define a custom Puppet resource named 'kill_process'
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'], # Include necessary paths
+  refreshonly => true,                 # Ensure it is only executed as needed
 }
+
+# Notify when the process is killed (optional)
+notify { 'Process killed':
+  subscribe => Exec['killmenow'],
+}
+
