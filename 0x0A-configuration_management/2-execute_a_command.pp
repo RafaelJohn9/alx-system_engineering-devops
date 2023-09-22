d6f348954f787630bd51ec1ee227b0541279900f
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-22 14:17:42 +0300

    correctd the command for task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,4 @@
 il# kills the process killmenow
 exec{'killmenow':
    command => 'pkill -f killmenow',
-   onlyif => 'pgrep -f killmenow',
-   path => '/usr/bin:/bin:/usr/sbin:/sbin',
-   refreshonly => true,
 }
