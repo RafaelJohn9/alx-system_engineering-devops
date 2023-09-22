2bd32a0c3e68e9577019a2a1e7af46fa74b579fb
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-22 21:46:35 +0300

    small changes in task 1 and 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# installs flask version 2.1.0
-package{ 'flask':
+# Using Puppet it installs flask version 2.1.0 from pip3
+
+package { 'Flask':
    ensure   => '2.1.0',
    provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,5 @@
 # Define a custom Puppet resource named 'kill_process'
 exec { 'killmenow':
   command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'], # Include necessary paths
-  refreshonly => true,                 # Ensure it is only executed as needed
+  provider    => 'shell',
 }
-
-# Notify when the process is killed (optional)
-notify { 'Process killed':
-  subscribe => Exec['killmenow'],
-}
-
