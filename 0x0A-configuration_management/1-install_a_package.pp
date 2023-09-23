74e0010e57f778e254c15215abc970cbe4f005b1
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-23 12:12:24 +0300

    puppeteer task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-# Using Puppet it installs flask version 2.1.0 from pip3
+# Using Puppet, install flask from pip3
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  name     => 'flask==2.1.0',
 }
-
