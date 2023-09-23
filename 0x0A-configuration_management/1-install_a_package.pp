94a8d18386caa8197326e5f55951ea67bcfd3f1b
RafaelJohn9 <rafaeljohb@gmail.com>
2023-09-23 03:12:28 +0300

    changes to task 1 and 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet it installs flask version 2.1.0 from pip3
 
-package { 'Flask':
+package { 'flask':
    ensure   => '2.1.0',
    provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # Define a custom Puppet resource named 'kill_process'
+
 exec { 'killmenow':
   command     => 'pkill -f killmenow',
   provider    => 'shell',
