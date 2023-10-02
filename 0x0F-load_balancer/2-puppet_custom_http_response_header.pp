7d5abe58f8caccaf168d47fd9a1903b5ae93520a
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-02 22:35:51 +0300

    corrected puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,31 @@
 # configuration of nginx
-exec { 'apt-update':
+
+$hostname = $::hostname
+
+exec { 'apt_update':
   command     => 'apt-get -y update',
   path        => '/usr/bin:/bin',
   refreshonly => true,
 }
 
-exec { 'apt-get':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin:/bin',
-  require => Exec['apt-update'],
+package { 'nginx':
+  ensure => 'installed',
 }
 
--> file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
 
-file_line { 'add_redirect':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => "\tlocation /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}",
-  notify => Exec['restart_nginx'],
+file { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  match  => 'listen 80 default_server',
+  line   => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
-file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "^\tlocation / {",
-  line   => "\t\tadd_header X-Served-By \"${::hostname}\";",
-  notify => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => File_line['add_redirect'],
+exec { 'restart service'
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
