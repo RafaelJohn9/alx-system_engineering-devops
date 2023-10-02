3dac878b94ea3be0f36d73853423d3011613da5a
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-02 22:47:34 +0300

    updated the script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,26 @@
-# configuration of nginx
+# automate the task of creating a custom HTTP header response
 
-exec { 'apt_update':
-  command     => 'apt-get -y update',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
+$hostname = $::hostname
+
+exec {'apt-update':
+    command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure => 'installed',
+package {'nginx':
+    ensure => 'installed',
 }
-
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
+    ensure  => 'running',
+    enable  => true,
+    require => Package['nginx'],
 }
-
 file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  content => "# Your entire file content here, including any existing content\n add_header X-Served-By \"${hostname}\";\n",
-  require => Package['nginx'],  # Ensure nginx package is installed first
-  notify  => Exec['restart service'],  # Trigger restart if the file changes
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    match => 'listen 80 default_server',
+    line  => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
 exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+    command  => 'sudo service nginx restart',
+    provider => shell,
 }
