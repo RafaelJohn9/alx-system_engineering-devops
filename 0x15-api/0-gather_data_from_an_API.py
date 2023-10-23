f8c31ec7d9d9690364827d37692db2170acde0b7
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-23 18:35:17 +0300

    task 0 :qpi

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+a python script that using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+if __name__ == "__main__":
+    """
+    this the unimportable script that gets the data
+    """
+    import requests
+    import sys
+
+    # declaration of variables
+    usrId = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+    noOfTasks = 0
+    tasksDone = 0
+    titlesOfTasks = []
+
+    # fully variables  definition
+    userUrl = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                           .format(usrId))
+    todosUrl = requests.get(f"https://jsonplaceholder.typicode.com/todos")
+    userData = userUrl.json()
+    todosData = todosUrl.json()
+
+    username = userData["name"]
+    for task in todosData:
+        if task["userId"] == usrId:
+            noOfTasks += 1
+            if task["completed"]:
+                titlesOfTasks.append(task["title"])
+                tasksDone += 1
+        else:
+            continue
+
+    # output
+    print(f"Empoyee {username} is done with tasks({tasksDone}/{noOfTasks}):")
+    for title in titlesOfTasks:
+        print("\t ", title)
