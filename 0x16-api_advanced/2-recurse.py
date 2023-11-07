f30d4a106b6ec06d8e7aa74dadb3ea36f0cedefa
RafaelJohn9 <rafaeljohb@gmail.com>
2023-11-07 21:23:03 +0300

    minor change in the header

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,7 +14,7 @@ def recurse(subreddit, hot_list=[], after=None):
     """
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Used for retrieving all title in subreddits'}
     params = {'limit': 100, 'after': after}
 
     response = requests.get(url, headers=headers, params=params)
