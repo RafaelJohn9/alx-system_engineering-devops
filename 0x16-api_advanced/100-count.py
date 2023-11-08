947253e2d9dc5baf3a03dbe91fe88c5ad9996244
RafaelJohn9 <rafaeljohb@gmail.com>
2023-11-08 08:50:36 +0300

    fixed and no use of loops

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,56 +2,55 @@
 """
 a module that  sontaints the count_words fucntion
 """
-import re
 import requests
+import re
 
 
-def count_words(subreddit, word_list, wordCount={}, after_token=""):
-    """
-    Count the occurrences of words in the titles of hot posts in a subreddit.
-    """
+def count_words(subreddit, word_list, wordCount=None, after_token=None):
     if subreddit is None:
         return None
 
-    # Construct the URL for Reddit's hot posts in the specified subreddit
-    base_url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if wordCount is None:
+        wordCount = {}  # Initialize wordCount dictionary
 
-    # Define a user agent to make the API request
+    if after_token is None:
+        after_token = ""  # Initialize after_token as an empty string
+
+    # Base URL and User Agent
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Gets the trending data"}
 
     # Make an HTTP GET request to the Reddit API
     response = requests.get(
-                           base_url, params={"after": after_token},
-                           headers=headers)
+        base_url, params={"after": after_token},
+        headers=headers
+    )
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Retrieve the 'after' token for pagination
-        after_token = response.json().get("data").get("after")
+    if response.status_code != 200:
+        return None
 
-        # If there are no more posts to fetch, print the word counts and exit
-        if not after_token:
-            sorted_wordCount = sorted(wordCount.items(),
-                                      key=lambda x: (-x[1], x[0]))
+    # Retrieve the 'after' token for pagination
+    after_token = response.json().get("data").get("after")
 
-            for word, occurrence in sorted_wordCount:
-                print('{}: {}'.format(word, occurrence))
-            return
+    # Process each post in the response
+    for post in response.json().get("data").get("children"):
+        # Iterate through the list of words to count
+        for word in word_list:
+            # Use regex to find occurrences of the word in the post title
+            pattern = re.escape(word.lower())
+            target = post.get('data').get('title').lower()
+            occur_count = len(re.findall(r'\b{}\b'.format(pattern), target))
+            if occur_count > 0:
+                # Update the word counts dictionary
+                wordCount[word.lower()] = (
+                        wordCount.get(word.lower(), 0) + occur_count)
 
-        # Process each post in the response
-        for post in response.json().get("data").get("children"):
-            # Iterate through the list of words to count
-            for word in word_list:
-                # Use regex to find occurrences of the word in the post title
-                pattern = re.escape(word.lower())
-                target = post.get('data').get('title').lower()
-                occur_count = len(re.findall(r'\b{}\b'.
-                                  format(pattern), target))
-                if occur_count > 0:
-                    # Update the word counts dictionary
-                    wordCount[word.lower()] = (
-                            wordCount.get(word, 0) + occur_count
-                            )
-
-        # Continue to the next page of posts
+    # If there are more posts to fetch, continue with the next page
+    if after_token:
         return count_words(subreddit, word_list, wordCount, after_token)
+
+    # If there are no more posts, print the word counts and return
+    sorted_wordCount = sorted(wordCount.items(), key=lambda x: (-x[1], x[0]))
+    for word, occurrence in sorted_wordCount:
+        print('{}: {}'.format(word, occurrence))
+    return wordCount
